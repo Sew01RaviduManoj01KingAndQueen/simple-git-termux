f4838c6fc2fbd705e844c796a756389c93c4a04f
Ravindu-Manoj <86040559+Sew01RaviduManoj01KingAndQueen@users.noreply.github.com>
2021-08-04 13:13:56 +0530

    Add files via upload

--- wlc.py
+++ wlc.py
@@ -1,8 +1,8 @@
 import os,sys,time,random
 print("")
 print("")
-color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"☬☞WelCome To Cyber World☜☬ \n\nOWNER:★★★★☠ THBD ☠★★★★ \n"
+color = ["\033[1;32m","\033[1;32m"]
+m = random.choice(color)+"☬☞WelCome To Cyber World☜☬ \nOWNER:★★★★☠THBD☠★★★★\n┈┈╭━╱▔▔▔▔╲━╮┈┈┈ꜱ\n┈┈╰╱╭▅╮╭▅╮╲╯┈┈┈ᴇ\n╳┈┈▏╰┈▅▅┈╯▕┈┈┈┈ᴡ\n┈┈┈╲┈╰━━╯┈╱┈┈╳┈ǫ\n┈┈┈╱╱▔╲╱▔╲╲┈┈┈┈ᴜ\n┈╭━╮▔▏┊┊▕▔╭━╮┈╳ᴇ\n┈┃┊┣▔╲┊┊╱▔┫┊┃┈┈ᴇ\n┈╰━━━━╲╱━━━━╯┈╳ɴ"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
