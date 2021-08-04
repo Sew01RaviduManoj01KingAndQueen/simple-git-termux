e12f41c806d05b91b38da4d495d9fce46b2c3b1c
Ravindu-Manoj <86040559+Sew01RaviduManoj01KingAndQueen@users.noreply.github.com>
2021-08-04 11:20:49 +0530

    👑KingAndQueen❤Sew❤Ravindu👑

--- wlc.py
+++ wlc.py
@@ -2,7 +2,7 @@ import os,sys,time,random
 print("")
 print("")
 color = ["\033[1;31m","\033[1;32m"]
-m = random.choice(color)+"Welcome THBD \n"
+m = random.choice(color)+"OWNER: THBD \n"
 for msg in m:
     sys.stdout.write(msg)
     sys.stdout.flush()
